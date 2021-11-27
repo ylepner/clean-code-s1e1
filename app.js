707760b955d0aee536b44b1de95f74d7393d7820
Yulia Lepner <ylepner@gmail.com>
2021-11-27 15:44:46 +0100

    refactor: change tag's selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder = document.getElementById("completed-tasks");//complete
 var createNewTaskElement = function (taskString) {
 
     var listItem = document.createElement("li");
+    listItem.classList.add('items-li')
 
     //input (checkbox)
     var checkBox = document.createElement("input");//checkbx
@@ -31,9 +32,11 @@ var createNewTaskElement = function (taskString) {
     //button.delete
     var deleteButton = document.createElement("button");//delete button
     var deleteButtonImg = document.createElement("img");//delete button image
+    deleteButtonImg.classList.add('remove-img')
 
     label.innerText = taskString;
     label.className = 'task';
+    label.classList.add('label-task')
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
